2738c68c7392b5d1c6b566f81f329f85362be09c
abhi808shek <abhi808gupta@gmail.com>
2023-04-08 11:58:27 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./Components/Navbar"
+
+
+
+const App = () => {
+  return (
+    <div className='w-10 h-10 bg-green-900'>
+      <div className='bg-red-500'><Navbar /></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='font-bold bg-red-500'>Top Courses</h1>
+    </div>
+  )
+}
+
+export default Navbar;
